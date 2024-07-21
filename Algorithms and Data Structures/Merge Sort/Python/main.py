66dcd0e7fbf351a3e5e13c9371c609a9e203e318
nhcoding <nhcoding@protonmail.com>
2024-07-22 00:37:52 +0100

    fixed typehints

--- Algorithms and Data Structures/Merge Sort/Python/main.py
+++ Algorithms and Data Structures/Merge Sort/Python/main.py
@@ -1,12 +1,12 @@
-from typing import List
+from typing import Any
 import random
 
 # ----------------------------------------------------------------------------------------------------------------------
 
 
 def main() -> None:
-    unsorted_data: List[int] = [random.randint(0, 101) for _ in range(100)]
-    sorted_data: List[int] = merge_sort(unsorted_data)
+    unsorted_data = [random.randint(0, 101) for _ in range(100)]
+    sorted_data: list[int] = merge_sort(unsorted_data)
     print(unsorted_data)
     print(sorted_data)
 
@@ -14,26 +14,27 @@ def main() -> None:
 # ----------------------------------------------------------------------------------------------------------------------
 
 
-def merge_sort(data: List[any]) -> List[any]:
-    """ Split data down into single parts. """
+def merge_sort(data: list[Any]) -> list[Any]:
+    """Split data down into single parts."""
 
     if len(data) == 1:
         return data
 
-    left: List[any] = merge_sort(data[int(len(data) / 2):])
-    right: List[any] = merge_sort(data[:int(len(data) / 2)])
+    left: list[Any] = merge_sort(data[int(len(data) / 2) :])
+    right: list[Any] = merge_sort(data[: int(len(data) / 2)])
 
     return sort_data(left, right)
 
 
 # ----------------------------------------------------------------------------------------------------------------------
 
-def sort_data(left: List[any], right: List[any]) -> List[any]:
-    """ Order data from lowest to highest. """
+
+def sort_data(left: list[Any], right: list[Any]) -> list[Any]:
+    """Order data from lowest to highest."""
 
     i: int = 0
     j: int = 0
-    sorted_data: List[any] = []
+    sorted_data: list[Any] = []
 
     while i < len(left) and j < len(right):
         if left[i] <= right[j]:
